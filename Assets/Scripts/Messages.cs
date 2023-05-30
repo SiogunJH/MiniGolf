b1ff06977941c79bcf80ea6936ed6ef0a3fa837a
SiogunJH <homarofficial@gmail.com>
2023-05-30 19:08:57 +0200

    Minor

--- Assets/Scripts/Messages.cs
+++ Assets/Scripts/Messages.cs
@@ -16,11 +16,6 @@ namespace Messages
             "Remember, staying on track is like being the DJ of your own life playlist. Keep the beats flowing and the dance moves grooving.",
             "Stay on track, because veering off-road might lead to unexpected encounters with talking animals who give questionable life advice.",
 
-            //Philosophical
-            "Amidst the ever-changing tides, remember that staying on course cultivates a sense of inner harmony.",
-            "Amidst the chaos of possibilities, staying focused ignites the spark of intention, illuminating the way forward.",
-            "When distractions arise and temptations abound, anchoring yourself to your goals ensures steady progress.",
-
             //Snarky
             "Oh, sure, go ahead and take the scenic route. It's not like we have a destination or anything.",
             "Breaking free from the shackles of direction sounds positively exhilarating!",
